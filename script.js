92d2b6f9d01544c1a8a771a2649ab03c7e9f18d8
petekgithub <petekshamitbeyli@gmail.com>
2022-03-16 23:30:18 +0300

    Final

--- script.js
+++ script.js
@@ -46,7 +46,7 @@ function showData(data) {
         <ul class="songs">
             ${data.data.map(song => `<li>
             <span><strong>${song.artist.name}</strong> - ${song.title}</span>
-            <button class="btn" data-artist="${song.artist.name}" data-songtitle="${song.title}" >Get Lyrics</button>
+            <button class="btn" data-artist="${song.artist.name}" data-songtitle="${song.title}">Get Lyrics</button>
         </li>`)
         .join('')
     }
@@ -80,6 +80,26 @@ async function getMoreSongs(url) {
   }
 
 
+// Get lyrics for songs
+async function getLyrics(artist, songTitle) {
+  const res = await fetch(`${apiURL}/v1/${artist}/${songTitle}`);
+    const data = await res.json();
+
+    const lyrics = data.lyrics.replace(/(\r\n|\r|\n)/g, '<br>');
+
+    result.innerHTML = `<h2><strong>${artist}</strong> - ${songTitle}</h2>
+    <span>${lyrics}</span>
+    `;
+
+    more.innerHTML = '';
+
+
+}
+
+
+
+
+
 // Event Listeners
 form.addEventListener('submit', e => {
     e.preventDefault();
@@ -92,4 +112,17 @@ form.addEventListener('submit', e => {
         searchSongs(searchTerm);
     }
     
+});
+
+// Get lyrics button click
+result.addEventListener('click', e => {
+  const clickedEl = e.target;
+
+  if(clickedEl.tagName === 'BUTTON') {
+    const artist = clickedEl.getAttribute('data-artist');
+    const songTitle= clickedEl.getAttribute('data-songtitle');
+
+    getLyrics(artist, songTitle); 
+
+  }
 });
